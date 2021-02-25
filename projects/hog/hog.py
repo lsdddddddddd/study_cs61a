e309f442912943412d71dd65315c89d36cec26e2
LSD <m527798157@gmail.com>
2021-02-25 21:12:10 +0800

    2021-2-25

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -150,6 +150,7 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
+        say = say(score0,score1)
     "*** YOUR CODE HERE ***"
     # END PROBLEM 6
     return score0, score1
@@ -237,6 +238,21 @@ def announce_highest(who, previous_high=0, previous_score=0):
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     # BEGIN PROBLEM 7
     "*** YOUR CODE HERE ***"
+    def say(score0,score1):
+        previous_score1 = previous_score
+        previous_high1 = previous_high
+        if who == 1:
+            if score1 - previous_score1 > previous_high1 :
+                previous_high1 = score1 - previous_score1
+                print(previous_high1 , "point(s)! That's the biggest gain yet for Player 1")
+            previous_score1 = score1
+        else:
+            if score0 - previous_score1 > previous_high1 :
+                previous_high1 = score0 - previous_score1
+                print(previous_high1 , "point(s)! That's the biggest gain yet for Player 0")
+            previous_score1 = score0
+        return announce_highest(who,previous_high1,previous_score1)
+    return say        
     # END PROBLEM 7
 
 
